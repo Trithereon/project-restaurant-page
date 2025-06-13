78403288ff4d7e96ac31db31ded4ba6550973d2d
Trithereon <eliottrassa@gmail.com>
2025-06-13 15:24:09 -0400

    Update Webpack for use with images

--- webpack.config.js
+++ webpack.config.js
@@ -15,15 +15,19 @@ module.exports = {
     }),
     ],
     module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
+        rules: [
+        {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+        },
+        {
+            test: /\.html$/i,
+            loader: "html-loader",
+        },
+        {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: "asset/resource",
+        },
     ],
-    },
+  },
 };
